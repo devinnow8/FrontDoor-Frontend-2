435ca99b776b4d0ab38a464cabdcbeb84a58a622
Sahil <devinnow8@gmail.com>
2023-04-11 17:09:19 +0530

    baseApi change

--- src/Api/BaseAPI.tsx
+++ src/Api/BaseAPI.tsx
@@ -2,22 +2,12 @@ import axios from "axios";
 
 let baseUrl = "http://localhost:3000/";
 
-async function post(endpointURL: any, data: any, config: any) {
-  return new Promise(async (resolve) => {
-    const response = await axios.post(`${baseUrl}${endpointURL}`, data, config);
-    if (response) {
-      resolve(response);
-    }
-  });
+function post(endpointURL: any, data: any, config: any) {
+  return axios.post(`${baseUrl}${endpointURL}`, data, config);
 }
 
-async function get(endpointURL: any, config: any) {
-  return new Promise(async (resolve) => {
-    const response = await axios.get(`${baseUrl}${endpointURL}`, config);
-    if (response) {
-      resolve(response);
-    }
-  });
+function get(endpointURL: any, config: any) {
+  return axios.get(`${baseUrl}${endpointURL}`, config);
 }
 
 export default { post, get };
